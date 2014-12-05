04b242151a506e328e29c25bd79a30ac7311da05
tkmagesh <tkmagesh77@gmail.com>
2014-12-05 14:51:30 +0530

    Using Streams

--- Day-01/03-WebServer/server.js
+++ Day-01/03-WebServer/server.js
@@ -2,13 +2,12 @@ var http = require("http"),
 	fs = require("fs"),
 	path = require("path");
 
-console.log("current folder = ", __dirname);
 var server = http.createServer(function(req,res){
 	var resource = req.url === "/" ? "/index.html" : req.url;
 	var resourcePath = path.join(__dirname, resource);
 	var fileExists = fs.existsSync(resourcePath);
 	if (fileExists){
-		fs.readFile(resourcePath, {encoding : "utf8"}, function(err,fileContents){
+		/*fs.readFile(resourcePath, {encoding : "utf8"}, function(err,fileContents){
 			if (err){
 				res.statusCode = 500;
 				res.end();
@@ -16,7 +15,16 @@ var server = http.createServer(function(req,res){
 				res.write(fileContents);
 				res.end();
 			}
+		});*/
+		//The above code unnecessirily caches the file contents before invoking your callback.  instead 'streams' can be used to write the file contents (chunk) to the response as and when they are read from the file
+		var stream = fs.createReadStream(resourcePath, {encoding : "utf8"});
+		/*stream.on("data", function(chunk){
+			res.write(chunk);
 		});
+		stream.on("end", function(){
+			res.end();
+		});		*/
+		stream.pipe(res);
 	} else {
 		res.statusCode = 404;
 		res.end();
