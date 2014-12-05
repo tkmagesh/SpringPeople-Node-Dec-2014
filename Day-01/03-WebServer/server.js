a2a82411d349c6411bd5a818a9920678bba711bd
tkmagesh <tkmagesh77@gmail.com>
2014-12-05 16:43:27 +0530

    Day - 1 Complete

--- Day-01/03-WebServer/server.js
+++ Day-01/03-WebServer/server.js
@@ -2,6 +2,7 @@ var http = require("http"),
 	fs = require("fs"),
 	url = require("url"),
 	calculator = require("./calculator"),
+	qs = require("querystring"),
 	path = require("path");
 
 var staticResourceExtns = [".html", ".htm", ".css", ".js", ".txt", ".ico", ".jpg", ".png", ".xml", ".json"];
@@ -11,6 +12,7 @@ var isStaticResource = function(resourceName){
 }
 
 var server = http.createServer(function(req,res){
+	console.log(req.method);
 	var resource = req.url === "/" ? "/index.html" : req.url;
 	//parse the requrl 
 	req.url = url.parse(req.url, true);
@@ -26,13 +28,31 @@ var server = http.createServer(function(req,res){
 			res.end();
 		}
 	} else if (reqPathName === "/calculator"){
-		var data = req.url.query,
-			number1 = parseInt(data.number1,10),
-			number2 = parseInt(data.number2,10);
-
-		var result = calculator[data.operation](number1,number2);
-		res.write(result.toString());
-		res.end();
+		if (req.method==="GET"){
+			var data = req.url.query,
+				number1 = parseInt(data.number1,10),
+				number2 = parseInt(data.number2,10);
+			if (typeof calculator[data.operation] === "function"){
+				var result = calculator[data.operation](number1,number2);
+				res.write(result.toString());
+			}
+			res.end();
+		} else if (req.method === "POST"){
+			var inputData = '';
+			req.on("data", function(chunk){
+				inputData += chunk;
+			});
+			req.on("end", function(){
+				var data = qs.parse(inputData),
+					number1 = parseInt(data.number1,10),
+					number2 = parseInt(data.number2,10);
+				if (typeof calculator[data.operation] === "function"){
+					var result = calculator[data.operation](number1,number2);
+					res.write(result.toString());
+				}
+				res.end();
+			});
+		}
 
 	}
 });
